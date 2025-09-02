bc45d4585bf7e090b7c89f8fd6c7deca765ecd53
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-09-02 08:25:41 +0100

    working with function based view in django

--- content/models.py
+++ content/models.py
@@ -13,12 +13,22 @@ class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, blank=True)
     department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='departments')
+    
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
     product = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+    
 
 class ProdDesc(models.Model):
     product = models.OneToOneField(Product, on_delete=models.CASCADE)
     description = models.TextField()
     
+    def __str__(self):
+        return self.name
--- content/views.py
+++ content/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Product
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Welcome to My Company")
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, "product_list.html", {"products": products})
